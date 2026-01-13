29a16d548bcf513b6ee2bfa49770ffb5baf865d6
Guste <219.guste@gmail.com>
2026-01-13 12:47:59 +0100

    feat: add HomeComponent and configure module federation
    - Created HomeComponent with its template, styles, and test file.
    - Updated AppModule to declare HomeComponent.
    - Added bootstrap file for module initialization.
    - Modified main.ts to import bootstrap module.
    - Updated TypeScript configuration to include all TypeScript files and exclude spec files.
    - Added Webpack configuration for module federation with remote entry.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'transactions',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './Routes',
+      }).then((m) => m.TransactionsRoutingModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
